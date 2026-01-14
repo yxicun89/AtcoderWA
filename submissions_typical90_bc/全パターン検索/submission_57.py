7cca200c036782a5e56663c00ff4cc242c7b6989
YUKI\yxicu <yxicun89@gmail.com>
2026-01-14 17:25:39 +0900

    jfjf

--- submissions_typical90_bc/全パターン検索/submission_57.py
+++ submissions_typical90_bc/全パターン検索/submission_57.py
@@ -1,48 +1,26 @@
 # 全探索
 import sys
 
-
-
 def solve():
-
     # 入力の読み込み
-
     N, P, Q = map(int, sys.stdin.readline().split())
-
     A = list(map(int, sys.stdin.readline().split()))
 
-
-
     # DPテーブルの初期化
-
     # dp[i][r]: i 個選んだときに積の余りが r になる選び方の数
-
     dp = [[0] * P for _ in range(6)]
-
     dp[0][0] = 1  # 何も選ばないときの初期状態
 
-
-
     # 数列をループしてDPを更新
-
     for a in A:
-
         # 逆順で更新（i=5から0へ）
-
         for i in range(4, -1, -1):
-
             for r in range(P):
-
                 if dp[i][r] > 0:  # 有効な状態のみ更新
-
                     new_r = (r * a) % P
-
                     dp[i + 1][new_r] += dp[i][r]
 
-
-
     # 答えを出力
-
     print(dp[5][Q])
 
 
--- submissions_typical90_bo/出力間違い/submission_21.py
+++ submissions_typical90_bo/出力間違い/submission_21.py
@@ -1,40 +1,21 @@
 # 最後の出力で8進数ではなく10進数に返却しちゃってる
 
 def to_base9(n: int) -> str:
-
     if n == 0:
-
         return "0"
-
     digit = ""
-
     while n:
-
         digit = str(n % 9) + digit
-
         n //= 9
-
     return digit
 
-
-
 N, K = map(int, input().split())
-
-# 最初のNは8進数として解釈する
-
 n = int(str(N), 8)
 
-
-
 for _ in range(K):
-
     s = to_base9(n)
-
     s = s.replace("8", "5")
-
-    n = int(s)  # ここは整数として保持しておく
-
-
+    n = int(s)
 
 print(s)
 
